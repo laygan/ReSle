2633fb01b084b400ba9c84cf55269a51308d0d6c
Tatsuya Kujimichi <amder.mf.gt@gmail.com>
2013-09-09 18:52:30 +0900

    パスワードを送信しないように変更

--- js/auth.js
+++ js/auth.js
@@ -3,6 +3,8 @@ function auth() {
     var passwd = document.getElementById("passwd").value;
     
     if (passwd != "" && uname != "") {
+        document.getElementById("passwd0").value="rgt4hyjuk8j7uj65h4tyju54hg3htjyruk6r5j;6hetyrku5j46hertejysku5e7j64";
+        document.getElementById("passwd1").value="k9l8o7ki6j5h4thyukilo;7p:-0p9oi7w46u3a5ehtjyrkutil6r76857jryku6il8r";
         var path = convert(uname, passwd);
         make_hidden('pass', path, 'signin');
         ocument.getElementById("passwd").value="";
@@ -23,11 +25,10 @@ function append() {
         window.alert("ユーザ名またはパスワードが入力されていません。");
     } else {
         if (pass0 == pass1) {
+            document.getElementById("passwd0").value="rgt4hyjuk8j7uj65h4tyju54hg3htjyruk6r5j;6hetyrku5j46hertejysku5e7j64";
+            document.getElementById("passwd1").value="k9l8o7ki6j5h4thyukilo;7p:-0p9oi7w46u3a5ehtjyrkutil6r76857jryku6il8r";
             var path = convert(uname, pass1);
-            window.alert(path);
             make_hidden('pass', path, 'signup');
-            document.getElementById("passwd0").value="";
-            document.getElementById("passwd1").value="";
             return true;
             exit(0);
         } else {
